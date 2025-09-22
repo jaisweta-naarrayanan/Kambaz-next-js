6100c4022b917b24d497b190a274d3f9ff449114
Jay <jsweta1707@gmail.com>
2025-09-22 18:34:23 -0400

    second commit

--- app/(Kambaz)/Courses/[1234]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[1234]/Assignments/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Assignments() {
   return (
     <div id="wd-assignments">
@@ -9,26 +10,26 @@ export default function Assignments() {
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A1 - ENV + HTML
-            </a> 
+            </Link> 
             <br/>Multiple Modules | <strong>Not available until</strong> May 6 at 12:00am |         
             <br/><strong>Due</strong> May 13 at 11:59pm | 100 pts 
         </li>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A2 - CSS + BOOTSTRAP
-          </a> 
+          </Link> 
           <br/>Multiple Modules | <strong>Not available until</strong> May 13 at 12:00am |
           <br/><strong>Due</strong> May 20 at 11:59pm | 100 pts
         </li>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A3 - JAVASCRIPT + REACT
-          </a> 
+          </Link> 
           <br/>Multiple Modules | <strong>Not available until</strong> May 20 at 12:00am |
           <br/><strong>Due</strong> May 27 at 11:59pm | 100 pts
       </li>
