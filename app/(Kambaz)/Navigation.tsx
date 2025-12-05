7466eb30a3becc07c870cc8e4f14a6864dfb471e
Jay <jsweta1707@gmail.com>
2025-12-04 19:46:23 -0500

    minor remote fixes

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -10,14 +10,14 @@ export default function KambazNavigation() {
   const pathname = usePathname();
   const links = [
     { label: "Dashboard", path: "/Dashboard", icon: AiOutlineDashboard },
-    { label: "Courses",   path: "/Dashboard", icon: LiaBookSolid },
-    { label: "Calendar",  path: "/Calendar",  icon: IoCalendarOutline },
-    { label: "Inbox",     path: "/Inbox",     icon: FaInbox },
-    { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+    { label: "Courses", path: "/Courses", icon: LiaBookSolid },
+    { label: "Calendar", path: "/Calendar", icon: IoCalendarOutline },
+    { label: "Inbox", path: "/Inbox", icon: FaInbox },
+    { label: "Labs", path: "/Labs", icon: LiaCogSolid },
   ];
   return (
-    <ListGroup id="wd-kambaz-navigation" style={{width: 120}}
-         className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+    <ListGroup id="wd-kambaz-navigation" style={{ width: 120 }}
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
       <ListGroupItem id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
         action className="bg-black border-0 text-center">
         <img src="/images/NEU.png" width="75px" /></ListGroupItem>
@@ -33,11 +33,12 @@ export default function KambazNavigation() {
         <ListGroupItem key={link.path} as={Link} href={link.path}
           className={`bg-black text-center border-0
               ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
-          {link.icon({ className: "fs-1 text-danger"})}
+          {link.icon({ className: "fs-1 text-danger" })}
           <br />
           {link.label}
         </ListGroupItem>
       ))}
     </ListGroup>
-);}
+  );
+}
 
